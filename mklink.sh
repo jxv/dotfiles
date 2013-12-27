ea6bcd4aa018be03e744b9b43bc1b9d8114336bb
jxv <joevargas@hush.com>
2013-12-27 14:16:54 -0800

    add bin folder, force remove looping/duplicate folders

--- mklink.sh
+++ mklink.sh
@@ -2,15 +2,21 @@
 
 ENV=$(pwd)
 
+# bin
+ln -sf $ENV/bin ~/.bin
+rm -rf $ENV/bin/bin
+
 # fontconfig
 mkdir -p ~/.config
 ln -sf $ENV/fontconfig ~/.config/fontconfig
+rm -rf $ENV/fontconfig/fontconfig
 
 # ghci
 ln -sf $ENV/ghc/ghci ~/.ghci
 
 # irssi
 ln -sf $ENV/irssi ~/.irssi
+rm -rf $ENV/irssi/irssi
 
 # vim
 ln -sf $ENV/vim/vimrc ~/.vimrc
@@ -21,6 +27,7 @@ ln -sf $ENV/x11/Xresources ~/.Xresources
 
 # xmonad
 ln -sf $ENV/xmonad/ ~/.xmonad/
+rm -rf $ENV/xmonad/xmonad
 
 # xmobar
 ln -sf $ENV/xmobar/xmobarrc ~/.xmobarrc
